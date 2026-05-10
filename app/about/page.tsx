e5ae54091b189600e184be495c6fe44e21dd5ffc
dhuallanca <dzavaleta@hotmail.com>
2026-05-10 17:09:40 -0400

    Adding routes next js

---
+++ app/about/page.tsx
@@ -0,0 +1,6 @@
+// page should be the name of file in order to use NextJs Router, which use folder name to create routes
+export default function About() {
+  return (
+    <div>About page</div>
+  );
+}
